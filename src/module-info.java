fd0446a8be5708017fbd721f293288338927741c
noless3011 <botay301104@gmail.com>
2024-04-13 12:08:53 +0700

    new commit

--- src/module-info.java
+++ src/module-info.java
@@ -2,10 +2,20 @@ module NewsScrapper {
 	exports model;
 	exports controller;
 	exports main;
-
+	requires transitive javafx.graphics;
+	opens controller;
+	requires org.jsoup;
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.fxml;
-	requires transitive javafx.graphics;
-	opens controller;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.web;
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.seleniumhq.selenium.support;
+	requires dev.failsafe.core;
+	requires com.google.gson;
+	requires com.google.common;
+	opens model to com.google.gson;
+	
 }
