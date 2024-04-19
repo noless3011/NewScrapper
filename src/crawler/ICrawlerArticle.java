8c813cd3ae370cdf960f0b33491e68af1b9c018a
noless3011 <botay301104@gmail.com>
2024-04-19 17:28:22 +0700

    added 1 progress calculating function to the crawler interface, make the crawl function allow people to enter the number of article/tweet/...

--- src/crawler/ICrawlerArticle.java
+++ src/crawler/ICrawlerArticle.java
@@ -6,8 +6,11 @@ import model.Content;
 import model.Article;
 
 public abstract interface ICrawlerArticle {
+
+	//Hàm này sẽ trả về 1 số float trong khoảng từ 0-1, cho biết đã crawl được bao nhiêu phần trăm
+	public float progress();
 	//Hàm này sẽ thu thập 1 lượng article mà người dùng nhập vào tron UI
-	public abstract void crawlArticleList();
+	public abstract void crawlArticleList(int amount);
 	//Need to check if the Article already exist to not have to store a duplicate using the id of the article
 	public abstract void saveToJson(List<Article> list);
 	public abstract List<Article> getArticlesFromJson();
--- src/crawler/ICrawlerTweet.java
+++ src/crawler/ICrawlerTweet.java
@@ -6,8 +6,10 @@ import model.Article;
 import model.Tweet;
 
 public interface ICrawlerTweet {
+	//Hàm này sẽ trả về 1 số float trong khoảng từ 0-1, cho biết đã crawl được bao nhiêu phần trăm
+	public float progress();
 	//Hàm này sẽ thu thập 1 lượng article mà người dùng nhập vào tron UI
-	public abstract void crawlTweetList();
+	public abstract void crawlTweetList(int amount);
 	//Need to check if the tweet already exist to not have to store a duplicate using the id of the article
 	public abstract void saveToJson(List<Article> list);
 	public abstract List<Tweet> getTweetFromJson();
