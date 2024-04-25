69ef3f2fc55cadf16f9cf556dfe9b9df59a87dde
noless3011 <botay301104@gmail.com>
2024-04-25 12:28:00 +0700

    Add progress callback class and add more feature to the interface

--- src/crawler/ICrawlerArticle.java
+++ src/crawler/ICrawlerArticle.java
@@ -2,15 +2,13 @@ package crawler;
 
 import java.util.List;
 
+import adapter.ProgressCallback;
 import model.Content;
 import model.Article;
 
-public abstract interface ICrawlerArticle {
-
-	//Hàm này sẽ trả về 1 số float trong khoảng từ 0-1, cho biết đã crawl được bao nhiêu phần trăm
-	public float progress();
-	//Hàm này sẽ thu thập 1 lượng article mà người dùng nhập vào tron UI
-	public abstract void crawlArticleList(int amount);
+public abstract interface ICrawlerArticle{
+	//Hàm này sẽ thu thập 1 lượng article mà người dùng nhập vào tron UI, parameter callback dùng để biết đc tiến độ của crawler hiện tại
+	public abstract void crawlArticleList(int amount, ProgressCallback callback);
 	//Need to check if the Article already exist to not have to store a duplicate using the id of the article
 	public abstract void saveToJson(List<Article> list);
 	public abstract List<Article> getArticlesFromJson();
