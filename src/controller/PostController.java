685f304a948de2ecea53aa7f75faa8a631202c4a
Daviseraaa <trantuandoan04@gmail.com>
2024-05-22 14:10:27 +0700

    hoan thien controller twitter

--- src/controller/PostController.java
+++ src/controller/PostController.java
@@ -15,6 +15,9 @@ public class PostController{
     private Label authorLabel;
 
     @FXML
+    private Label tagAuthor;
+    
+    @FXML
     private Label cmtLabel;
 
     @FXML
@@ -51,21 +54,28 @@ public class PostController{
         shareLabel.setText(post.getNumber_of_share());
     }
 
+    // Hiển thị cho tweet
     public void setData(Tweet post){
         authorLabel.setText(post.getAuthor());
+        tagAuthor.setText("@" + post.getAuthor());
         timeLabel.setText(timeParse(post.getPublishedAt()));
-        contentLabel.setText(post.getContent().toString());
         
-        //String imgURL = post.getImgUrl();
-        //if(imgURL.isEmpty()) {
-        	//image.setImage(new Image("https://i.pinimg.com/564x/eb/c9/af/ebc9afde8c2b05bbf639cfc1c56dc59a.jpg"));
-        //}else {
-           // image.setImage(new Image(imgURL));
-        //}
+        String[] parts = post.getContent().toString().split("\\{url=");
+    	String imgURL = "";
+    	for (int i = 1; i < parts.length; i++) {
+    		imgURL = parts[i].split(", description=")[0];
+    	}
+        
+        contentLabel.setText(parts[0]);        
+        if(!imgURL.isEmpty()) {
+        	image.setImage(new Image(imgURL));
+        }
 
         reactionLabel.setText(post.getNumber_of_liked());
         cmtLabel.setText( post.getNumber_of_comment());
-        viewLabel.setText(post.getNumber_of_view());
+        viewLabel.setText("· " + post.getNumber_of_view() + " Views");
+        
+        
     }
     
     private String timeParse(LocalDateTime input){
